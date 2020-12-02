a7a3b07d10b457edac901f4e30dfbff120af904d
Hugo <64895940+Madara-sensei@users.noreply.github.com>
2020-12-02 23:10:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import firebase from'firebase'
 require('firebase/firestore');
 
   var firebaseConfig = {
-    apiKey: "AIzaSyC_fealR2Z-1Cn5q3zPLn3kNZoDNSFGjtw",
-    authDomain: "simplylink.firebaseapp.com",
-    databaseURL: "https://simplylink.firebaseio.com",
-    projectId: "simplylink",
-    storageBucket: "simplylink.appspot.com",
-    messagingSenderId: "153360537276",
-    appId: "1:153360537276:web:9834e71e0e4d37260a62b9",
-    measurementId: "G-79C60G2EQQ"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
